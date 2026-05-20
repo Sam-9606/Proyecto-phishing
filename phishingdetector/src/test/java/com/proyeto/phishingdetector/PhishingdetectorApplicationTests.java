22825d8d62210befb95e0b73e076393f5acfc716
Sam-9606 <luisamuel.toconasparra@gmail.com>
2026-05-20 03:00:03 -0500

    primer commit tras creacion del repositorio
    actualmente es solo el lienzo que nos deja spring initializer

---
+++ phishingdetector/src/test/java/com/proyeto/phishingdetector/PhishingdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyeto.phishingdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
