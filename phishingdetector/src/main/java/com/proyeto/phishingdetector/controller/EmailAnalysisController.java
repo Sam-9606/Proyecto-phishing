f6e415a6ed2964292c9b2d847955c756ba5addd2
Sam-9606 <luisamuel.toconasparra@gmail.com>
2026-05-24 10:26:03 -0500

    mejora de respuesta http
    da los típicos "errores" 200, 400, 404 cuando todo funciona corretamente, no se envian datos validos y se solicita un dato inexistente respectivamente

--- phishingdetector/src/main/java/com/proyeto/phishingdetector/controller/EmailAnalysisController.java
+++ phishingdetector/src/main/java/com/proyeto/phishingdetector/controller/EmailAnalysisController.java
@@ -3,6 +3,7 @@ package com.proyeto.phishingdetector.controller;
 import com.proyeto.phishingdetector.model.EmailAnalysis;
 import com.proyeto.phishingdetector.service.EmailAnalysisService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -23,28 +24,66 @@ public class EmailAnalysisController {
     }
     
     @GetMapping("/{id}")
-    public EmailAnalysis getById(@PathVariable Long id) {
-        return service.getById(id);
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+
+        try {
+            return ResponseEntity.ok(service.getById(id));
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity
+                    .status(404)
+                    .body(e.getMessage());
+        }
     }
     
     @DeleteMapping("/{id}")
-    public String deleteById(@PathVariable Long id) {
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
 
-        service.deleteById(id);
+        try {
 
-        return "Análisis eliminado correctamente";
+            service.deleteById(id);
+
+            return ResponseEntity.ok("Análisis eliminado correctamente");
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity
+                    .status(404)
+                    .body(e.getMessage());
+        }
     }
     
     @PutMapping("/{id}")
-    public EmailAnalysis update(
+    public ResponseEntity<?> update(
             @PathVariable Long id,
             @RequestBody EmailAnalysis emailAnalysis) {
 
-        return service.update(id, emailAnalysis);
+        try {
+
+            return ResponseEntity.ok(service.update(id, emailAnalysis));
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity
+                    .status(404)
+                    .body(e.getMessage());
+        }
     }
     
     @PostMapping
-    public EmailAnalysis analyzeEmail(@RequestBody EmailAnalysis emailAnalysis) {
-        return service.analyzeAndSave(emailAnalysis);
+    public ResponseEntity<?> analyzeEmail(@RequestBody EmailAnalysis emailAnalysis) {
+
+        try {
+            EmailAnalysis saved = service.analyzeAndSave(emailAnalysis);
+
+            return ResponseEntity.ok(saved);
+
+        } catch (RuntimeException e) {
+
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
     }
 }
